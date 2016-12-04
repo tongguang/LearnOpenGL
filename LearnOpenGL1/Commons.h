a94efd316dee1f4f202a3b8bf0ade1d38231949b
星光 <591912867@qq.com>
2016-12-04 19:49:44 +0800

    Revert "测试"
    This reverts commit 54d17c4a5546e672031da2ca6f82469acdd60180.

--- LearnOpenGL1/Commons.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// GLEW
-#define GLEW_STATIC
-#include <GL/eglew.h>
-
-// GLFW
-#include <GLFW/glfw3.h>
--- LearnOpenGL1/MainWindow.h
+++ LearnOpenGL1/MainWindow.h
@@ -1,16 +1,1 @@
 #pragma once
-
-#include "Commons.h"
-
-class CMainWindow
-{
-public:
-	CMainWindow(); 
-	~CMainWindow();
-	static void InitInstance();
-
-private:
-	GLFWwindow* _window;
-
-	static CMainWindow* s_Instance;
-};
