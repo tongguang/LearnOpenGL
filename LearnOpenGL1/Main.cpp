c6b44097e3d3d5ecfbd5dd099e78060796420e5b
星光 <591912867@qq.com>
2017-01-02 23:50:09 +0800

    删除无用代码

--- LearnOpenGL1/Main.cpp
+++ LearnOpenGL1/Main.cpp
@@ -1,6 +1,4 @@
 #include "MainWindow.h"
-#include "Tool.h"
-#include <iostream>
 
 void DestroyInstance()
 {
@@ -9,7 +7,6 @@ void DestroyInstance()
 
 int main()
 {
-	std::cout << ReadFileData("D:\\Learn\\LearnOpenGL\\LearnOpenGL1\\ceshi.txt") << endl;
 	auto mainWindow = CMainWindow::GetInstance();
 	mainWindow->Run();
 	DestroyInstance();
